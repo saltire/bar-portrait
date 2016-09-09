bfd09d2548d0cd4b9ddf08d9961ec88b339937f6
saltire sable <saltiresable@gmail.com>
2016-09-09 16:22:53 -0400

    Refactor script into object format

--- js/bars.js
+++ js/bars.js
@@ -1,42 +1,56 @@
 'use strict';
 
-const rows = 20;
-const columns = 20;
-const curviness = 0.5;
-const barHeight = 0.8;
+const BarPortrait = function (canvasId, imagePath, rows, columns, curviness, barHeight) {
+    // Create main canvas.
+    this.canvas = document.getElementById(canvasId);
+    this.ctx = this.canvas.getContext('2d');
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+    // Create image canvas.
+    this.imgCanvas = document.createElement('canvas');
+    this.imgCanvas.width = this.canvas.width;
+    this.imgCanvas.height = this.canvas.height;
+    this.imgCtx = this.imgCanvas.getContext('2d');
 
-ctx.fillStyle = 'black';
+    // Initialize variables.
+    this.rows = rows;
+    this.columns = columns;
+    this.curviness = curviness;
+    this.barHeight = barHeight;
 
-const image = new Image();
-image.src = './cube.png';
-image.onload = () => {
-    const imgCanvas = document.createElement('canvas');
-    imgCanvas.width = canvas.width;
-    imgCanvas.height = canvas.height;
-    const imgCtx = imgCanvas.getContext('2d');
-    const ratio = Math.min(canvas.width / image.width, canvas.height / image.height);
-    imgCtx.scale(ratio, ratio);
-    imgCtx.drawImage(image, 0, 0);
+    this.setImage(imagePath);
+};
 
-    const data = imgCtx.getImageData(0, 0, canvas.width, canvas.height).data;
+BarPortrait.prototype.setImage = function (imagePath) {
+    const image = new Image();
+    image.src = imagePath;
+    image.onload = () => {
+        const ratio = Math.min(this.canvas.width / image.width, this.canvas.height / image.height);
+        this.imgCtx.setTransform(ratio, 0, 0, ratio, 0, 0);
+        this.imgCtx.drawImage(image, 0, 0);
 
-    const xSpacing = (canvas.width - 1) / columns;
-    const ySpacing = (canvas.height - 1) / rows;
-    const cpLength = curviness * xSpacing;
-    const maxHeight = barHeight * ySpacing;
+        this.draw();
+    };
+};
+
+BarPortrait.prototype.draw = function () {
+    this.ctx.fillStyle = 'black';
+
+    const data = this.imgCtx.getImageData(0, 0, this.canvas.width, this.canvas.height).data;
+
+    const xSpacing = (this.canvas.width - 1) / this.columns;
+    const ySpacing = (this.canvas.height - 1) / this.rows;
+    const cpLength = this.curviness * xSpacing;
+    const maxHeight = this.barHeight * ySpacing;
 
     for (let y = 0; y < canvas.height; y += ySpacing) {
         const baseline = y + ySpacing - 1;
         let lastHeight = baseline;
 
-        ctx.beginPath();
-        ctx.moveTo(0, baseline);
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, baseline);
 
-        for (let x = 0; x <= canvas.width; x += xSpacing) {
-            const p = (Math.round(y + ySpacing / 2) * canvas.width + Math.round(x)) * 4;
+        for (let x = 0; x <= this.canvas.width; x += xSpacing) {
+            const p = (Math.round(y + ySpacing / 2) * this.canvas.width + Math.round(x)) * 4;
             const r = data[p];
             const g = data[p + 1];
             const b = data[p + 2];
@@ -45,23 +59,25 @@ image.onload = () => {
             const height = baseline - (255 - v) / 255 * maxHeight;
 
             if (x === 0) {
-                ctx.lineTo(x, height);
+                this.ctx.lineTo(x, height);
             }
             else {
-                ctx.bezierCurveTo(
+                this.ctx.bezierCurveTo(
                     x - xSpacing + cpLength, lastHeight,
                     x - cpLength, height,
                     x, height);
             }
 
             if (x === canvas.width - 1) {
-                ctx.lineTo(x, baseline);
+                this.ctx.lineTo(x, baseline);
             }
 
             lastHeight = height;
         }
 
-        ctx.closePath();
-        ctx.fill();
+        this.ctx.closePath();
+        this.ctx.fill();
     }
 };
+
+new BarPortrait('canvas', 'cube.png', 20, 20, 0.5, 0.8);
